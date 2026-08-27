5ff1fde73a509261236b6c24f7ed3a55875de28e
Joao-Goncalves10 <joao.l.goncalves@aluno.senai.br>
2026-08-27 15:43:55 -0300

    ⚙️Organização dos arquivos

--- my-app/App.js
+++ my-app/App.js
@@ -1,26 +1,6 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import AeroButton from './src/components/AeroButton'; // Importando o componente
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* Botão ativo (ícone azul destacado) */}
-      <AeroButton name="home" focused={true} />
-
-      {/* Botão inativo */}
-      <AeroButton name="search" focused={false} />
-    </View>
-  );
+  return <HomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#CDE5FF', // Fundo claro estilo Frutiger Aero para testar o efeito de transparência
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    gap: 15,
-  },
-});
